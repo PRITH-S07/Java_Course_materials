bb1f88d54161752c420bb54bc23bf9f0f9f068cb
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-01 23:36:47 +0530

    Create anonymousarrays.java

---
+++ Loops_controls_arrays/anonymousarrays.java
@@ -0,0 +1,14 @@
+package example;
+import java.util.*;
+import java.lang.Math;
+public class Hello {
+    public static void main(String args[])
+    {
+        display(new int[] {2,3,4,5});
+    }
+    public static void display(int m[])
+    {
+        for(int k:m)
+            System.out.println(k);
+    }
+}
