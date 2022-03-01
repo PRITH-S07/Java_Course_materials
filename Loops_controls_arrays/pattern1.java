f89fbfba26c242d49662863e399b8279af995cc3
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-01 22:27:27 +0530

    Create pattern1.java

---
+++ Loops_controls_arrays/pattern1.java
@@ -0,0 +1,14 @@
+package example;
+import java.util.*;
+import java.lang.Math;
+public class Hello {
+    public static void main(String args[])
+    {
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<=i;j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
