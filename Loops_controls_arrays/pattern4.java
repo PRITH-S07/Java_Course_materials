ae45fd8b6d71a1d32529465c1823ee045611903a
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-01 22:33:00 +0530

    Create pattern4.java

---
+++ Loops_controls_arrays/pattern4.java
@@ -0,0 +1,16 @@
+package example;
+import java.util.*;
+import java.lang.Math;
+public class Hello {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=i;j>=1;j--)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
