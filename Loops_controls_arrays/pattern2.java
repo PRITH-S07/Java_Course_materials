3317d647cd2d991e8ec89dd6f755da3b3abc2194
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-01 22:29:03 +0530

    Create pattern2.java

---
+++ Loops_controls_arrays/pattern2.java
@@ -0,0 +1,16 @@
+package example;
+import java.util.*;
+import java.lang.Math;
+public class Hello {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
