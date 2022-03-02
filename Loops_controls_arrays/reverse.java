8c352fdbfe1aee38bcb34ad3ff01628979a00aeb
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-02 16:40:51 +0530

    Create reverse.java

---
+++ Loops_controls_arrays/reverse.java
@@ -0,0 +1,21 @@
+package example;
+import java.util.*;
+import java.lang.Math;
+public class Hello {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++)
+            arr[i]=sc.nextInt();
+        for(int i=0;i<n/2;i++)
+        {
+            int temp=arr[i];
+            arr[i]=arr[n-i-1];
+            arr[n-i-1]=temp;
+        }
+        for(int k:arr)
+            System.out.println(k);
+    }
+}
