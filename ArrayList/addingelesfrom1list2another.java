ca701bfa1d1ee01431b578e5defb6fd076bef732
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-03-27 19:15:38 +0530

    Create addingelesfrom1list2another.java

---
+++ ArrayList/addingelesfrom1list2another.java
@@ -0,0 +1,27 @@
+package example;
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+public class practice
+{
+    public static void main(String[] args) throws Exception
+    {
+        ArrayList <String> s=new ArrayList<String>();
+        s.add("Ram");
+        s.add("Jeff");
+        s.add(1,"Chris");//to insert a particular element at an index
+        ArrayList <String> name=new ArrayList<String>(s);
+        /*System.out.println("Size of ArrayList is:"+s.size());//to get size of ArrayList*/
+        ListIterator <String> itr=name.listIterator();
+        while(itr.hasNext())
+        {
+            String j=(String)itr.next();
+            System.out.println(j);
+        }
+    }
+}
+
+/*Output:
+Ram
+Chris
+Jeff*/
