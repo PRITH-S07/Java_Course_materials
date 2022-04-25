781aa30f3652520b100e11cb3e23776ae770071c
PRITH-S07 <58857629+PRITH-S07@users.noreply.github.com>
2022-04-25 10:18:07 +0530

    Create hyperlink.java

---
+++ JavaFX/hyperlink.java
@@ -0,0 +1,25 @@
+package com.example.myjavafx;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.*;
+import java.io.*;
+import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+public class HelloApplication extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Hyperlink yt=new Hyperlink("https://www.youtube.com");
+        VBox root=new VBox(yt);
+        Scene sc=new Scene(root);
+        primaryStage.setHeight(500);
+        primaryStage.setWidth(500);
+        primaryStage.setScene(sc);
+        primaryStage.show();
+    }
+}
